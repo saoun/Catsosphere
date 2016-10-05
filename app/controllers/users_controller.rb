55deff0e48293955656cbbd8d3c08fd3781cace9
sarah aoun <sarah@silk.co>
2016-10-05 17:24:16 -0400

    make basic navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
 
-  def create
+  def index
+    @users = User.all
+  end
 
+  def show
+    @user = User.find_by(id: params[:id])
   end
 
   def destroy
+    User.destroy(params[:id])
+    session[:user_id] = nil
+    redirect_to(:back)
+  end
 
+  def edit
+    @user = User.find_by_id(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to(:user)
   end
 end
