b39e0f3e0fd32857961414d5b8a0080e9270382f
sarah aoun <sarah@silk.co>
2016-10-05 17:06:20 -0400

    begin styling test

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root to: "users#index"
+  root to: "posts#index"
   get "/auth/:provider/callback" => "sessions#create"
   get "/signout" => "sessions#destroy", :as => :signout
 end
