a80cfc9464029d9ff9d169d033c74da4a7f6d36d
sarah aoun <sarah@silk.co>
2016-10-12 16:50:30 -0400

    devise workssss

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,12 +1,6 @@
-class User < ApplicationRecord
+class Model < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
-
-
-
 end
---
+++ db/migrate/20161012203415_add_info_to_users.rb
@@ -0,0 +1,7 @@
+class AddInfoToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :fullname, :string
+    add_column :users, :contact, :string
+    add_column :users, :bio, :string
+  end
+end
